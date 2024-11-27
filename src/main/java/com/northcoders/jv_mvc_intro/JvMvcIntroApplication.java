d1e22984e8ae4d96b7ac5e5a372e59aa3dc04eea
Vincent <va543567@gmail.com>
2024-11-27 10:44:01 +0000

    initial spring boot application

---
+++ src/main/java/com/northcoders/jv_mvc_intro/JvMvcIntroApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_mvc_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvMvcIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvMvcIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/jv_mvc_intro/JvMvcIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_mvc_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvMvcIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
